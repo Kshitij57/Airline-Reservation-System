96808602717dd6d478b0e2659114e59b7fd4a233
Kshitij umarkar <kshitijumarkar@gmail.com>
2023-06-21 11:45:01 +0530

    21 june first commit

---
+++ src/main/java/Entity/User.java
@@ -0,0 +1,17 @@
+package Entity;
+
+import java.util.Scanner ;
+public class User {
+
+	public static void main(String[]args) {
+		Scanner in = new Scanner(System.in);
+		
+		System.out.println("Welcome to Airline Reservation System");
+		
+	
+		System.out.println("Press 1 to Register");
+		System.out.println("Press 2  to Login");
+		System.out.println("Press 3  to Book Ticket");
+		
+	}
+}
